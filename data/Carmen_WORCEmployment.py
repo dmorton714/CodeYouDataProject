8d825042ff141b752f1881723e10f48bac8ce38c
luckycarms <caleha23@gmail.com>
2025-07-23 10:11:44 -0400

    updated to only cleaning info of column and null removals

--- data/Carmen_WORCEmployment.py
+++ data/Carmen_WORCEmployment.py
@@ -4,26 +4,6 @@ import os
 file_path = "data/WORC_Employment.xlsx"
 worc = pd.read_excel(file_path)
 
-def unique(df, column='column'):
-    """
-    Check for unique values in a specified DataFrame column.
-
-    Parameters:
-    df: The DataFrame that contains the data.
-    column: The name of the column to check for uniqueness.
-
-    Returns:
-    numpy.ndarray: An array of unique values in the specified column.
-    """
-    unique_values = df[column].unique()
-    print(f"Unique values in '{column}': {unique_values}")
-    
-    return unique_values
-
-unique(worc, 'ATP Placement Type')
-
-worc.isnull().any().any()
-
 cols_to_drop = ['Auto Id','Employment History Name']
 
 worc_cols_dropped = worc.drop(columns=cols_to_drop, axis=1)
