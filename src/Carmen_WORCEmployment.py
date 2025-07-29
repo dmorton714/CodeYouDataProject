7ac539d11d1fe724f7eff1014e7e658bff3bb6f1
luckycarms <caleha23@gmail.com>
2025-07-29 14:57:00 -0400

    removed or # out dropping of null values

--- src/Carmen_WORCEmployment.py
+++ src/Carmen_WORCEmployment.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-file_path = "data/WORC_Employment.xlsx"
+file_path = "../../data/WORC_Employment.xlsx"
 worc = pd.read_excel(file_path)
 
 # removed auto id as we may need it later
 cols_to_drop = ['Employment History Name']
 
-worc_cols_dropped = worc.drop(columns=cols_to_drop, axis=1)
+#worc_cols_dropped = worc.drop(columns=cols_to_drop, axis=1)
 
-# Why did we decide to drop all nulls?
-# This can be dangerous if we have a lot of nulls
+# Why did we decide to drop all nulls? I dropped nulls because there was no salary value. Which is what I thought was important for this data set. 
+# This can be dangerous if we have a lot of nulls 
 # Also it removed the entire row if any column had a null value
-# will this cause issues later?
-worc_cols_dropped_nulls = worc_cols_dropped.dropna()
+# will this cause issues later? I could see this being an issue with larger datasets. I have removed.
+# worc_cols_dropped_nulls = worc_cols_dropped.dropna()
 
-worc_cleaned = worc_cols_dropped_nulls
+worc_cleaned = worc_cols_dropped
